943387ad684dce7d58a42c54d8323243a37fc5c1
Indian Bittu <newdb29@gmail.com>
2025-07-28 18:03:19 +0530

    SpringBoot2-6: Added webapp code of app manage product

---
+++ Spring/Spring-Boot2/spring-boot-web-application-master/src/main/java/com/halim/web/model/Product.java
@@ -0,0 +1,61 @@
+package com.halim.web.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="prod")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name;
+	private String description;
+	private int price;
+	@Lob
+	@Column(columnDefinition = "BLOB")
+	private String image;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", description=" 
+	            + description + ", price=" + price + ", image="
+				+ image + "]";
+	}
+	
+
+}
